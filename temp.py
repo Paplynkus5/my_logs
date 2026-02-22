ada279cc56d4be82476e6427d030d0aad9cdf66a
Aleksander <48942302+Paplynkus5@users.noreply.github.com>
2026-02-22 16:16:15 +0000

    delete tempfile

--- temp.py
+++
@@ -1,6 +0,0 @@
-import time
-
-while True:
-    print('archived_logs\\merged\\' + '-'.join(map(str, time.localtime()[0:3])) + ' ' + ''.join(f'{time_element:02s}' for time_element in map(str, time.localtime()[3:6])) + '.log')
-    print('archived_logs\\merged\\' + '-'.join(f'{time_element:02d}' for time_element in time.localtime()[0:3]) + ' ' + ''.join(f'{time_element:02d}' for time_element in time.localtime()[3:6]) + '.log')
-    time.sleep(1)
